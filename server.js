59ef1c3e2d469203c37981c197cf935431c945f0
juan royo <ju.val.roy@gmail.com>
2020-05-20 16:48:36 +0200

    usando cors

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(cors());
-app.set('view engine', 'ejs');
+
 
 app.use('/api', createProxyMiddleware({ target: 'https://zylenstudio.herokuapp.com', changeOrigin: true }));
 
