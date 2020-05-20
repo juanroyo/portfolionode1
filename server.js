f54af9687476c50e8b5337fa3ee663b73b8a7135
juan royo <ju.val.roy@gmail.com>
2020-05-20 14:59:39 +0200

    cors puede que aya

--- server.js
+++ server.js
@@ -47,13 +47,16 @@ async function run() {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({ type: 'application/json' }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://zylenstudio.herokuapp.com'
+}));
 app.set('view engine', 'ejs');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.all('/', function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   next()
+ });
 
 //-------------CART----------------
 app.post("/cart", (req, res) => {
